06372c7c6d9975d13c7c0b023dc9b6c907c4c4ef
jofarc <jofarc789@gmail.com>
2025-03-06 01:32:56 -0800

    inital commit

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
+
+export default defineConfig({
+  plugins: [react(), nodePolyfills()],
+  server: {
+    port: 3005,
+  },
+  resolve: {
+    alias: {
+      "@assets": "/src/assets",
+      "@features": "/src/features",
+      "@pages": "/src/pages",
+      "@styles": "/src/styles",
+      "@utils": "/src/utils",
+      "@constants": "/src/constants",
+      "@context": "/src/context",
+      "@middleware": "/src/middleware",
+      "@hooks": "/src/hooks",
+    },
+  },
+});
